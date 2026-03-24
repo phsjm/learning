a3ccc707862f9677fb1e075c4cb519d8f64baecf
phsjm <pchann.0624@gmail.com>
2026-03-24 23:23:41 +0900

    IDをキーに要素を取得する

---
+++ dom/main.js
@@ -0,0 +1,6 @@
+'use strict';
+const e = document.getElementById('button');
+e.addEventListener('click', () => {
+    const result = document.getElementById('result');
+    console.log(result.innerText);
+});
